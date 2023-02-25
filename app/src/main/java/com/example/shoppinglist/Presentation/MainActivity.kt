f64041644cf3c9009b48c8ef23519db079031274
roman <aregsa2@gmail.com>
2023-02-25 15:49:07 +0200

    was added viewModel for main activity

--- app/src/main/java/com/example/shoppinglist/Presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/Presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoppinglist
+package com.example.shoppinglist.Presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
